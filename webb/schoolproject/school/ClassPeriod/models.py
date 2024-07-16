d4cf20499c0262ce4e3ee682968d0a877d0fb1e5
Mutesi-alin <alinemutesiii@gmail.com>
2024-07-16 09:35:35 +0300

    add file

--- webb/schoolproject/school/ClassPeriod/models.py
+++ webb/schoolproject/school/ClassPeriod/models.py
@@ -2,15 +2,10 @@ from django.db import models
 
 class ClassPeriod(models.Model):
     start_time = models.TimeField()
-end_time = models.TimeField() 
-classroom = models.ForeignKey('Classroom', on_delete=models.CASCADE)
-day_of_week = models.CharField(max_length=3)
-course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    end_time = models.TimeField() 
+    classroom = models.CharField(max_length=23)
+    day_of_week = models.CharField(max_length=3)
 
-
-
-
-def __str__(self):
-     return f" {self.start_time} {self.end_time}"
-
+    def __str__(self):
+        return f"{self.start_time} - {self.end_time}"
 
--- webb/schoolproject/school/school/settings.py
+++ webb/schoolproject/school/school/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+BASE_DIR= Path (__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
     'classes',
     'course',
     'rest_framework',
+    'ClassPeriod',
 ]
 
 MIDDLEWARE = [
